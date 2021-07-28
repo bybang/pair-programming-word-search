81867538a0fc439fc169e3ebf65a181b8245e375
bybang <bybang26@gmail.com>
2021-07-28 17:29:39 -0400

    PP w2d3 word search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// PAIR PROGRAM - WORD SEARCH
+// ANTHONY KIM, JOSHUA BANG
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    const verticalArray = transpose(letters).map(letters => letters.join(''));
+  
+    for (const wordsV of verticalArray) {
+      if (wordsV.includes(word)) {
+        return true;
+      }
+    }
+      
+    for (const wordsH of horizontalJoin) {
+      if (wordsH.includes(word)) {
+        return true;
+      }
+    }
+  
+    return false;
+  };
+  
+  module.exports = wordSearch;
+  
+  //Callback
+  const transpose = function(matrix) {
+    let finalArray = [];
+    let stagingArray = [];
+    let counter = matrix[0].length;
+    while (counter > 0) {
+      for (const array of matrix) {
+        for (let i = 0; i < array.length; i++) {
+          stagingArray.push(array.shift());
+          break;
+        }
+      }
+      finalArray.push(stagingArray);
+      stagingArray = [];
+      counter--;
+    }
+    return finalArray;
+  };
+  
+    
